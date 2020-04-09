ece6a8515f02a74f1dd6aa4dce91e7966c481d17
user <nono647@gmail.com>
2020-04-09 18:13:55 +0200

    first commmit

---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+  <div class="container-fluid">
+    <nav class="float-left">
+      <ul>
+        <li>
+          <a href="#">
+              {{ __('link') }}
+          </a>
+        </li>
+        <li>
+          <a href="#">
+              {{ __('link1') }}
+          </a>
+        </li>
+        <li>
+          <a href="#">
+              {{ __('link2') }}
+          </a>
+        </li>
+        <li>
+          <a href="#">
+              {{ __('Link3') }}
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <div class="copyright float-right">
+      &copy;
+      <script>
+        document.write(new Date().getFullYear())
+      </script> <i class="material-icons"></i> by
+      <a href="#" target="_blank">text</a> and <a href="#" target="_blank">text</a> fb.
+    </div>
+  </div>
+</footer>
